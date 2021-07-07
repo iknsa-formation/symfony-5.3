98c9e0dad208b78d0f1f48f3ea248d0a372d1cf5
Khalid Sookia <khalidsookia@gmail.com>
2021-07-07 11:15:17 +0200

    adress

--- src/Controller/BasketController.php
+++ src/Controller/BasketController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionBagInterface;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\HttpFoundation\Session\Storage\SessionStorageInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BasketController extends AbstractController
@@ -15,18 +19,29 @@ class BasketController extends AbstractController
      */
     public function index(Request $request, ProductRepository $productRepository): Response
     {
-        $listBasket = [];
-
-        foreach ($request->request as $data) {
-            $listBasket[] = [
-                'product' => $productRepository->findOneBy(['id' => $data['product']]),
-                'quantity' => $data['quantity']
-            ];
+        $basket = $request->getSession()->get('listBasket');
+        if (!$basket) {
+            $request->getSession()->set('listBasket', []);
         }
+
+        if (!isset($basket[$request->request->get('product')])) {
+            $basket[$request->request->get('product')] = [
+                'product' => $productRepository->findOneBy(
+                    ['id' => $request->request->get('product')]
+                ),
+                'quantity' => $request->request->get('quantity')
+            ];
+        } else {
+            $basket[$request->request->get('product')]['quantity'] =
+                (int) $basket[$request->request->get('product')]['quantity'] +
+                (int) $request->request->get('quantity');
+        }
+
+        $request->getSession()->set('listBasket', $basket);
 
         return $this->render('basket/index.html.twig', [
             'controller_name' => 'BasketController',
-            'listBasket' => $listBasket
+            'listBasket' => $basket
         ]);
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
@@ -41,6 +43,16 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $resetPasswordToken;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Address::class, mappedBy="user", orphanRemoval=true)
+     */
+    private $addresses;
+
+    public function __construct()
+    {
+        $this->addresses = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -153,6 +165,36 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setResetPasswordToken(?string $resetPasswordToken): self
     {
         $this->resetPasswordToken = $resetPasswordToken;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Address[]
+     */
+    public function getAddresses(): Collection
+    {
+        return $this->addresses;
+    }
+
+    public function addAddress(Address $address): self
+    {
+        if (!$this->addresses->contains($address)) {
+            $this->addresses[] = $address;
+            $address->setUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeAddress(Address $address): self
+    {
+        if ($this->addresses->removeElement($address)) {
+            // set the owning side to null (unless already changed)
+            if ($address->getUser() === $this) {
+                $address->setUser(null);
+            }
+        }
 
         return $this;
     }
